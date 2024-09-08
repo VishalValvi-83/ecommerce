a98012c6f787f99f91ef8e79adbac981d2ae1599
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-09-08 19:00:50 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 import Home from './views/Home/Home'
 import About from './views/About/About'
-import Review from './views/Review/Review'
+// import Review from './views/Review/Review'
 import Login from './views/Login/Login'
 import Products from './views/Products/Products'
 import Offers from './views/Offers/Offers'
+import SignUp from './views/SignUp/SignUp'
+import Discus from './views/discussionforum/discuss'
 
 function App() {
   const router =createBrowserRouter([
     {
       path:'/',
-      element:<Home></Home>
+      element:<Home/>
     },
     {
       path:"/about",
-      element:<About></About>
+      element:<About/>
     },
     {
       path:"/login",
-      element:<Login></Login>
+      element:<Login/>
     },
     {
       path:"/products",
-      element:<Products></Products>
+      element:<Products/>
     },
     {
       path:"/offers",
-      element:<Offers></Offers>
+      element:<Offers/>
     },
     {
       path:"/signup",
-      element:<SignUp></SignUp>
+      element:<SignUp/>
     },
+    {
+      path :"/discuss",
+      element:<Discus/>
+    }
   ])
   
 
